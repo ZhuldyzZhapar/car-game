0916a58262265aef09c21be600e59d3a249d0a95
zzmalika <malika.zhusupbaeva@gmail.com>
2020-11-20 23:21:47 +0600

    clean code

--- cargame.py
+++ cargame.py
@@ -34,7 +34,7 @@ class Car:
 
 
 class Block:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -54,8 +54,8 @@ blocks = []
 scores = []
 
 block_speed = 6
-a=[]
-l = []
+num_of_obstacles = []
+num_of_coin = []
 direction = ""
 money = 0
 
@@ -73,72 +73,76 @@ while not done:
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         car.x += 5
     
-    k = pygame.time.get_ticks()%2000
+    k = pygame.time.get_ticks() % 2000
 
     screen.fill((65, 65, 65))
 
-    for i in range(163,419,64):
-        for j in range(0,720,20):
+    for i in range(163, 419, 64):
+        for j in range(0, 720, 20):
             if ((j+road_speed+10) % 700-(j+road_speed) % 700) < 0:
-                pygame.draw.line(screen, (255,255,255),(i,0),(i,(j+road_speed+10)%700),3)
+                pygame.draw.line(screen, (255, 255, 255), (i, 0), (i, (j+road_speed+10) % 700), 3)
             else:
                 pygame.draw.line(screen, (255, 255, 255), (i, (j+road_speed) % 700), (i, (j+road_speed+10) % 700), 3)
-    pygame.draw.line(screen, (255, 255, 255),(98, 0), (98, 700), 3)
-    pygame.draw.line(screen, (255, 255, 255), (418, 0),(418, 700), 3)
-    screen.blit(image_lawn,(0,0))
-    screen.blit(image_lawn,(420,0))
-    screen.blit(image_car,(car.x,car.y))
-    screen.blit(image_start,(212,road_speed))
+    pygame.draw.line(screen, (255, 255, 255), (98, 0), (98, 700), 3)
+    pygame.draw.line(screen, (255, 255, 255), (418, 0), (418, 700), 3)
+    screen.blit(image_lawn, (0, 0))
+    screen.blit(image_lawn, (420, 0))
+    screen.blit(image_car, (car.x, car.y))
+    screen.blit(image_start, (212, road_speed))
 
     if 1300 < k < 1340:
-        r = random.randint(0,4)
-        c = random.randint(0,4)
+        obstacle_line_appear = random.randint(0, 4)
+        coin_line_appear = random.randint(0, 4)
 
-        a.append(r)
-        l.append(c)
+        num_of_obstacles.append(obstacle_line_appear)
+        num_of_coin.append(coin_line_appear)
 
-        l=list(dict.fromkeys(l))
-        a=list(dict.fromkeys(a))
+        num_of_coin = list(dict.fromkeys(num_of_coin))
+        num_of_obstacles = list(dict.fromkeys(num_of_obstacles))
 
-        for i in a:
-            block = Block(place[i],0)
+        for i in num_of_obstacles:
+            block = Block(place[i], 0)
             blocks.append(block)
 
-        for i in l:
-            score = Score(place[i],0)
+        for i in num_of_coin:
+            score = Score(place[i], 0)
             scores.append(score)  
 
     for block in blocks:
-        screen.blit(image_obstacle,(block.x,block.y))
+        screen.blit(image_obstacle, (block.x, block.y))
         block.y += block_speed
-        if block.y>700:
+        if block.y > 700:
             blocks.remove(block)
-            a=[]
+            num_of_obstacles = []
         
-        if ((car.x > block.x and car.x < block.x + 40) and (car.y > block.y and car.y < block.y + 80)) or ((car.x +40 > block.x and car.x+40 < block.x + 40) and (car.y  > block.y and car.y < block.y + 80)):# or (block.y == block.x + 40):
+        if (block.x < car.x < block.x + 40 and block.y < car.y < block.y + 80) or ((car.x + 40 > block.x and car.x+40 <
+                                                                                    block.x + 40) and block.y < car.y <
+                                                                                   block.y + 80):
             print("CRASH")
-            print(block.x,car.x, block.y ,car.y)
-            road_speed=0
-            block_speed=0
+            print(block.x, car.x, block.y, car.y)
+            road_speed = 0
+            block_speed = 0
             crash()
     
     for score in scores:
-        screen.blit(image_coin,(score.x,score.y))
-        score.y+=block_speed
-        if score.y>700:
+        screen.blit(image_coin, (score.x, score.y))
+        score.y += block_speed
+        if score.y > 700:
             scores.remove(score)
-            a=[]
+            a = []
 
-        if ((car.x >= score.x and car.x <= score.x + 30) and (car.y >= score.y and car.y <= score.y + 30)) or ((car.x +40 >= score.x and car.x+40 <= score.x + 30) and (car.y  >= score.y and car.y <= score.y + 30)):
+        if ((score.x <= car.x <= score.x + 30) and (car.y <= car.y <= score.y + 30)) or ((score.x <= car.x + 40 <=
+                                                                                          score.x + 30) and score.y <
+                                                                                         car.y <= score.y + 30):
             print("\n++money!!!!!\n")
             money += 1
             scores.remove(score)
-    if (car.x > 0 and car.x < 98) or (car.x + 40 < 700 and car.x + 40 > 418): 
+    if (0 < car.x < 98) or (700 < car.x + 40 > 418):
         print("CRASH")
-        road_speed=0
+        road_speed = 0
         crash()
 
-    road_speed+=4
+    road_speed += 4
 
     pygame.display.flip()
     clock.tick(40)
